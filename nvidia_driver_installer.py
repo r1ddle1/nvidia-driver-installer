3a069ffb5bbc3a966d3eab9e3d0ed13dfd77cfd2
r1ddle <r1ddle@localhost.localdomain>
2019-12-31 18:58:04 +0300

    Remove print_beautifully function, add thonkdifferent to authors list, corrected tabulation

--- nvidia_driver_installer.py
+++ nvidia_driver_installer.py
@@ -6,18 +6,6 @@ UBUNTU = 'Ubuntu'
 FEDORA = 'Fedora'
 MANJARO = 'Manjaro'
 
-# COLORS
-COLOR_BLACK = 0
-COLOR_RED = 1
-COLOR_GREEN = 2
-COLOR_YELLOW = 3
-COLOR_MAGENTA = 5
-COLOR_CYAN = 6
-
-
-def print_beautifully(text, color):
-    os.system('tput setaf ' + str(color) + '; echo ' + str(text))
-
 def get_system_name():
     try:
         output = execute_shell_command(['lsb_release', '-a'])
@@ -39,7 +27,7 @@ def execute_shell_command(command):
 def is_running_as_root():
     output = execute_shell_command('whoami')
 
-    if output == 'root\n':  # Why '\n'? Because I don't know!
+    if output == 'root\n':  # Why '\n'? Because execute_shell_command(command) returns output with '\n' at the end!
         return True
     else:
         return False
@@ -65,27 +53,34 @@ def get_available_drivers(system_name):
                 drivers.append(output[i:i + 17])  # 17 - length of nvidia-driver-XXX
         return drivers
     elif system_name == FEDORA:
-        print("Checking RPMFusion-nvidia-nonfree") #Check for RPMFusion NVIDIA repo, and install it if not found
+        print("Checking RPMFusion-nvidia-nonfree")  # Check for RPMFusion NVIDIA repo, and install it if not found
         execute_shell_command(["dnf", "install", "fedora-workstation-repositories"])
         execute_shell_command(["dnf", "config-manager", "--set-enabled","rpmfusion-nonfree-nvidia-driver"])
-        #Now we should have RPMFusion installed and active
-        
+        # Now we should have RPMFusion installed and active
 
-
-
-
-        
 
 
 def main():
     is_root = is_running_as_root()
-    print("Running as root?:", is_root)
+    print("Running as root?: ", is_root)
 
     if not is_root:
         print("Error! Please run this program as root!")
         exit(-1)
     else:
         print('Good! Program is running as root!')
+
+    print('WARNING! THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, '
+    'EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, '
+    'FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT '
+    'HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, '
+    'TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.')
+
+    answer = input('Continue? [y/n]\n')
+
+    if answer != 'y':
+        print('Quitting...')
+        exit(0)
 
     system_name = get_system_name()
 
@@ -124,31 +119,39 @@ def main():
 
         print("We'll check for the RPMFusion repositories wich contain the drivers")
 
-        card_type = int(input("What NVIDIA card do you have?:\n Type 1, for recent GeForce/Quadro/Tesla\n Type 2 for legacy GeForce 400/500 \n Type 3 for Legacy GeForce 8/9/200/300"))
-        #Driver instalation
+        print('What NVIDIA card do you have?:')
+        print('Type 1, for recent GeForce/Quadro/Tesla')
+        print('Type 2 for legacy GeForce 400/500')
+        print('Type 3 for Legacy GeForce 8/9/200/300')
+        card_type = int(input())
 
+        # Driver instalation
         if card_type == 1:
             execute_shell_command(["dnf", "install", "akmod-nvidia"])
-        elif card_type == 2:#possibly broken, no way to test due to missing hardware(aka, I dont have an old GPU)
+        elif card_type == 2:  # Possibly broken, no way to test due to missing hardware (aka, I dont have an old GPU)
             execute_shell_command(["dnf", "install", "xorg-x11-drv-nvidia-390xx","akmod-nvidia-390xx"])
-        elif card_type == 3:#possibly broken, no way to test due to missing hardware(aka, I dont have an old GPU)
+        elif card_type == 3:  # Possibly broken, no way to test due to missing hardware (aka, I dont have an old GPU)
             execute_shell_command(["dnf", "install", "xorg-x11-drv-nvidia-340xx","akmod-nvidia-340xx"])
 
         print("Configuring boot splash(plymouth)")
-        
-        #After installing the drivers, you MUST make the drivers start at preboot(after GRUB) so the boot screen displays properly
-        
+
+        # After installing the drivers, you MUST make the drivers start at preboot (after GRUB) so the boot screen displays properly
+
         execute_shell_command(["echo", "\"options nvidia_drm modeset=1\" ", ">>", "/etc/modprobe.d/nvidia.conf"])
-        execute_shell_command(["echo", "add_drivers+=\"nvidia nvidia_modeset nvidia_uvm nvidia_drm\"\ninstall_items+=\"/etc/modprobe.d/nvidia.conf\"", ">>", "/etc/dracut.conf.d/nvidia.conf"])
+        execute_shell_command(["echo",
+                               "add_drivers+=\"nvidia nvidia_modeset nvidia_uvm nvidia_drm\"\ninstall_items+=\"/etc/modprobe.d/nvidia.conf\"", 
+                               ">>",
+                               "/etc/dracut.conf.d/nvidia.conf"])
         execute_shell_command(["dracut", "-f"])
 
         print('Okay. Installation finished. Please reboot your PC! Would you like to reboot your PC now? [y/n]')
+
         answer = input()
 
         if answer == 'y':
             os.system('reboot')
 
-    print_beautifully('Thank you for using my software! With love, r1ddle', COLOR_MAGENTA)
+    print('Thank you for using this software! Made by r1ddle & thonkdifferent')
 
 
 main()
