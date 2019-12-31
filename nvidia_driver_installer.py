8cbadab41f0a9924629dfb1956f1fb3583672fb1
thonkdifferent <41342923+thonkdifferent@users.noreply.github.com>
2019-12-31 17:01:52 +0200

    Fedora support
    Added Fedora support

--- nvidia_driver_installer.py
+++ nvidia_driver_installer.py
@@ -1,5 +1,6 @@
 import subprocess
 import os
+import re
 
 UBUNTU = 'Ubuntu'
 FEDORA = 'Fedora'
@@ -18,13 +19,16 @@ def print_beautifully(text, color):
     os.system('tput setaf ' + str(color) + '; echo ' + str(text))
 
 def get_system_name():
-    output = execute_shell_command(['lsb_release', '-a'])
-    if UBUNTU in output:
-        return UBUNTU
-    elif FEDORA in output:
-        return FEDORA
-    elif MANJARO in output:
-        return MANJARO
+    try:
+        output = execute_shell_command(['lsb_release', '-a'])
+        if UBUNTU in output:
+            return UBUNTU
+        elif FEDORA in output:
+            return FEDORA
+        elif MANJARO in output:
+            return MANJARO
+    except FileNotFoundError:
+        print("lsb_release not found. If you are on Fedora, install the redhat-lsb-core package")
 
 
 def execute_shell_command(command):
@@ -44,16 +48,34 @@ def is_running_as_root():
 def update_repositories(system_name):
     if system_name == UBUNTU:
         os.system('sudo apt update')
+    elif system_name == FEDORA:
+        os.system('sudo dnf update')
+    else:
+        print("UNSUPPORTED SYSTEM!")
+        exit()
 
 
-def get_available_drivers():
-    output = execute_shell_command(['apt','search', "NVIDIA driver metapackage"])
-    drivers = []
-    for i in range(len(output)):
-        if output[i] == 'n' and output[i + 1] == 'v' \
-        and output[i + 2] == 'i' and output[i + 3] == 'd':
-            drivers.append(output[i:i + 17])  # 17 - length of nvidia-driver-XXX
-    return drivers
+def get_available_drivers(system_name):
+    if system_name == UBUNTU:
+        output = execute_shell_command(['apt','search', "NVIDIA driver metapackage"])
+        drivers = []
+        for i in range(len(output)):
+            if output[i] == 'n' and output[i + 1] == 'v' \
+            and output[i + 2] == 'i' and output[i + 3] == 'd':
+                drivers.append(output[i:i + 17])  # 17 - length of nvidia-driver-XXX
+        return drivers
+    elif system_name == FEDORA:
+        print("Checking RPMFusion-nvidia-nonfree") #Check for RPMFusion NVIDIA repo, and install it if not found
+        execute_shell_command(["dnf", "install", "fedora-workstation-repositories"])
+        execute_shell_command(["dnf", "config-manager", "--set-enabled","rpmfusion-nonfree-nvidia-driver"])
+        #Now we should have RPMFusion installed and active
+        
+
+
+
+
+        
+
 
 def main():
     is_root = is_running_as_root()
@@ -74,7 +96,7 @@ def main():
         update_repositories(system_name)
 
         print('All right! Let\'s find some nvidia drivers...')
-        available_drivers = get_available_drivers()
+        available_drivers = get_available_drivers(system_name)
 
         print('Okay. I\'ve found', len(available_drivers), 'of them')
 
@@ -96,7 +118,35 @@ def main():
 
     elif system_name == FEDORA:
         print('Looks like you\'re using Fedora.')
+        print('Let\'s update your repositories!\n\n')
 
+        update_repositories(system_name)
+
+        print("We'll check for the RPMFusion repositories wich contain the drivers")
+
+        card_type = int(input("What NVIDIA card do you have?:\n Type 1, for recent GeForce/Quadro/Tesla\n Type 2 for legacy GeForce 400/500 \n Type 3 for Legacy GeForce 8/9/200/300"))
+        #Driver instalation
+
+        if card_type == 1:
+            execute_shell_command(["dnf", "install", "akmod-nvidia"])
+        elif card_type == 2:#possibly broken, no way to test due to missing hardware(aka, I dont have an old GPU)
+            execute_shell_command(["dnf", "install", "xorg-x11-drv-nvidia-390xx","akmod-nvidia-390xx"])
+        elif card_type == 3:#possibly broken, no way to test due to missing hardware(aka, I dont have an old GPU)
+            execute_shell_command(["dnf", "install", "xorg-x11-drv-nvidia-340xx","akmod-nvidia-340xx"])
+
+        print("Configuring boot splash(plymouth)")
+        
+        #After installing the drivers, you MUST make the drivers start at preboot(after GRUB) so the boot screen displays properly
+        
+        execute_shell_command(["echo", "\"options nvidia_drm modeset=1\" ", ">>", "/etc/modprobe.d/nvidia.conf"])
+        execute_shell_command(["echo", "add_drivers+=\"nvidia nvidia_modeset nvidia_uvm nvidia_drm\"\ninstall_items+=\"/etc/modprobe.d/nvidia.conf\"", ">>", "/etc/dracut.conf.d/nvidia.conf"])
+        execute_shell_command(["dracut", "-f"])
+
+        print('Okay. Installation finished. Please reboot your PC! Would you like to reboot your PC now? [y/n]')
+        answer = input()
+
+        if answer == 'y':
+            os.system('reboot')
 
     print_beautifully('Thank you for using my software! With love, r1ddle', COLOR_MAGENTA)
 
